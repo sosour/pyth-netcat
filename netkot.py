a78341f5a09a9546f24a1578e9246d8ce800d9a9
ag zy <agn.zygula@gmail.com>
2017-03-29 16:17:37 +0200

    Adding client_sender

--- netkot.py
+++ netkot.py
@@ -4,6 +4,7 @@ import socket
 import getopt
 import threading
 import subprocess
+import client_sender
 
 listen = False
 command = False
@@ -16,7 +17,7 @@ port = 0
 def usage():
     print "NETKOT"
     print
-    print "Usage: netkot.py target_host:port"
+    print "Usage: netkot.py -t target_host -p port"
     print "-l --listen			listen on [host]:[port]"
     print "-e --execute=file_to_run	execute file on connection"
     print "-c --command			initializes command line"
@@ -31,6 +32,47 @@ def usage():
     sys.exit(0)
 
 def main():
-    usage()
+    global listen 
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:",
+        ["help","listen","execute","target","port","command","upload"])  
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()  
+    for o,a in opts:
+        if o in ("-h", "--help"):
+	    usage()
+	elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-e", "--execute"):
+            execute = a
+        elif o in ("-t", "--target"):
+	    target = a
+        elif o in ("-p", "--port"):
+	    port = int(a)
+        elif o in ("-c", "--command"):
+      	    command = a
+        elif o in ("-u","--upload"):
+	    upload_destination = a
+	else:
+	    assert False,"Unsupported option"
+
+	if not listen and len(target) and port > 0:
+	    buffer = sys.stdin.read()
+
+            client_sender(buffer, target, port)
+
+	if listen:
+	    server_loop()
+
 
 main()
